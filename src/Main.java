eea680928b88afa5f9c40d62616571deb3af3a4c
Eder de Bastos <eder.bastos21@gmail.com>
2024-08-17 17:42:02 -0300

    make first calculator base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Create scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Declare variables outside of function scope
+        float result = 0;
+
+        //Get first number
+        System.out.print("Insert first number: ");
+        float firstNumber = scanner.nextFloat();
+
+        //Get operation
+        System.out.print("Insert operation: ");
+        String operation = scanner.next();
+
+        //Get second number
+        System.out.print("Insert second number: ");
+        float secondNumber = scanner.nextFloat();
+
+        //Process depending on user's operation choice
+        if (operation.equals("+")){
+            result = firstNumber + secondNumber;
+        } else if (operation.equals("-")){
+            result = firstNumber - secondNumber;
+        } else if (operation.equals("/")){
+            result = firstNumber / secondNumber;
+        } else if (operation.equals("*")) {
+            result = firstNumber * secondNumber;
+        }
+
+        //Output
+        System.out.println(result);
     }
 }
